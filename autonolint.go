b5b4ddc1f90b7ab7ce32fbb89135a2e81d879715
utgwkk <utagawakiki@gmail.com>
2025-01-16 01:05:07 +0900

    write out as string

--- autonolint.go
+++ autonolint.go
@@ -1,6 +1,8 @@
 package autonolint
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"go/ast"
@@ -8,7 +10,6 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
-	"slices"
 
 	"golang.org/x/tools/go/ast/astutil"
 )
@@ -30,7 +31,7 @@ type Pos struct {
 type InsertComment struct {
 	Filename string
 
-	Offset int
+	Line int
 
 	FromLinter string
 
@@ -61,27 +62,31 @@ func Process(issues []Issue) error {
 	}
 
 	for filename, cs := range rewriteByFile {
-		slices.SortFunc(cs, func(a, b *InsertComment) int {
-			return a.Offset - b.Offset
-		})
-		fset := token.NewFileSet()
-		f, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
-		if err != nil {
-			return fmt.Errorf("parser.ParseFile: %w", err)
-		}
-
+		buf := &bytes.Buffer{}
+		rewritesByLine := map[int][]*InsertComment{}
 		for _, c := range cs {
-			pos := token.Pos(c.Offset)
-			x := fset.File(f.Pos())
-			ls := x.LineStart(x.Line(pos))
-			f.Comments = append(f.Comments, &ast.CommentGroup{
-				List: []*ast.Comment{
-					{
-						Slash: ls,
-						Text:  c.Comment(),
-					},
-				},
-			})
+			rewritesByLine[c.Line] = append(rewritesByLine[c.Line], c)
+		}
+		in, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		s := bufio.NewScanner(in)
+		lineno := 1
+		for s.Scan() {
+			if cs, ok := rewritesByLine[lineno]; ok {
+				for _, c := range cs {
+					buf.WriteString(c.Comment() + "\n")
+				}
+			}
+			buf.WriteString(s.Text() + "\n")
+			lineno++
+		}
+		in.Close()
+
+		formatted, err := format.Source(buf.Bytes())
+		if err != nil {
+			return fmt.Errorf("format.Source: %w (%s)", err, buf.String())
 		}
 
 		out, err := os.Create(filename)
@@ -89,8 +94,8 @@ func Process(issues []Issue) error {
 			return fmt.Errorf("os.Create: %w", err)
 		}
 		defer out.Close()
-		if err := format.Node(out, fset, f); err != nil {
-			return fmt.Errorf("decorator.Fprint: %w", err)
+		if _, err := out.Write(formatted); err != nil {
+			return err
 		}
 	}
 
@@ -119,10 +124,11 @@ func processIssue(issue Issue) (*InsertComment, error) {
 	if p == nil {
 		return nil, errSkip
 	}
+	line := fset.File(1).Line(p.Pos())
 
 	return &InsertComment{
 		Filename:   issue.Pos.Filename,
-		Offset:     issue.Pos.Offset,
+		Line:       line,
 		FromLinter: issue.FromLinter,
 		Reason:     "test",
 	}, nil
