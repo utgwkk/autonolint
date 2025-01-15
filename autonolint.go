3907404f3b27b47b256718e21de08a3338996909
utgwkk <utagawakiki@gmail.com>
2025-01-15 22:05:40 +0900

    first implementation

--- autonolint.go
+++ autonolint.go
@@ -1,11 +1,13 @@
 package autonolint
 
 import (
+	"errors"
 	"fmt"
-	"go/ast"
 	"go/parser"
 	"go/token"
+	"os"
 
+	"github.com/dave/dst/decorator"
 	"golang.org/x/tools/go/ast/astutil"
 )
 
@@ -23,30 +25,92 @@ type Pos struct {
 	Column int
 }
 
+type InsertComment struct {
+	Filename string
+
+	Offset int
+
+	FromLinter string
+
+	Reason string
+}
+
+var errSkip = errors.New("skip")
+
+func (c *InsertComment) Comment() string {
+	if c.Reason != "" {
+		return "//nolint:" + c.FromLinter + " // " + c.Reason
+	}
+	return "//nolint:" + c.FromLinter
+}
+
 func Process(issues []Issue) error {
+	rewriteByFile := map[string][]*InsertComment{}
+
 	for _, issue := range issues {
-		if err := processIssue(issue); err != nil {
+		c, err := processIssue(issue)
+		if err != nil {
+			if errors.Is(err, errSkip) {
+				continue
+			}
 			return err
 		}
+		rewriteByFile[c.Filename] = append(rewriteByFile[c.Filename], c)
 	}
+
+	for filename, cs := range rewriteByFile {
+		fset := token.NewFileSet()
+		f, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+		if err != nil {
+			return fmt.Errorf("parser.ParseFile: %w", err)
+		}
+		dec := decorator.NewDecorator(fset)
+
+		for _, c := range cs {
+			pos := token.Pos(c.Offset)
+			path, _ := astutil.PathEnclosingInterval(f, pos, pos)
+			n, err := dec.DecorateNode(path[0])
+			if err != nil {
+				return fmt.Errorf("dec.DecorateNode: %w", err)
+			}
+
+			n.Decorations().End.Prepend(c.Comment())
+		}
+
+		ff, err := dec.DecorateFile(f)
+		if err != nil {
+			return fmt.Errorf("decorator.DecorateFile: %w", err)
+		}
+		out, err := os.Create(filename)
+		if err != nil {
+			return fmt.Errorf("os.Create: %w", err)
+		}
+		defer out.Close()
+		if err := decorator.Fprint(out, ff); err != nil {
+			return fmt.Errorf("decorator.Fprint: %w", err)
+		}
+	}
+
 	return nil
 }
 
-func processIssue(issue Issue) error {
+func processIssue(issue Issue) (*InsertComment, error) {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, issue.Pos.Filename, nil, parser.ParseComments)
 	if err != nil {
-		return fmt.Errorf("parser.ParseFile: %w", err)
+		return nil, fmt.Errorf("parser.ParseFile: %w", err)
 	}
 	pos := token.Pos(issue.Pos.Offset + 1)
 	path, _ := astutil.PathEnclosingInterval(f, pos, pos)
 	if len(path) == 0 {
-		return nil
+		return nil, errSkip
 	}
 	p := path[0]
-	ast.Print(fset, p)
 
-	// TODO: insert //nolint:xxx comment before p
-
-	return nil
+	return &InsertComment{
+		Filename:   issue.Pos.Filename,
+		Offset:     int(p.Pos()),
+		FromLinter: issue.FromLinter,
+		Reason:     "",
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/utgwkk/autonolint
 go 1.23.2
 
 require golang.org/x/tools v0.29.0
+
+require (
+	github.com/dave/dst v0.27.3
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+)
